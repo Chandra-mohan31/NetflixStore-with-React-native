968c0da6478ff7768098b4095eee430c9aa97907
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2020-11-17 22:29:06 +0530

    netflix store

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./screens/Home";
+import Edit from "./screens/Edit";
+import Add from "./screens/Add";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <NavigationContainer>
+         <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen 
+            name="Home"
+            component={Home}
+            options={{
+              headerStyle:{
+                backgroundColor:"#0f4c75"
+              },
+              title:'LCO netflix app',
+              headerTitleStyle:{
+                textAlign:"center",
+                color:"white"
+              }
+            }}
+            >
+
+            </Stack.Screen>
+            <Stack.Screen 
+            name="Add"
+            component={Add}
+            options={{
+              headerStyle:{
+                backgroundColor:"#0f4c75"
+              },
+              title:'LCO netflix app',
+              headerTitleStyle:{
+                textAlign:"center",
+                color:"white"
+              }
+            }}
+            >
+
+            </Stack.Screen>
+            <Stack.Screen 
+            name="Edit"
+            component={Edit}
+            options={{
+              headerStyle:{
+                backgroundColor:"#0f4c75"
+              },
+              title:'LCO netflix app',
+              headerTitleStyle:{
+                textAlign:"center",
+                color:"white"
+              }
+            }}
+            >
+
+            </Stack.Screen>
+         </Stack.Navigator>
+      </NavigationContainer>
+     
+      
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
+
+
