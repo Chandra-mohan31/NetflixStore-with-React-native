5dfa9fe6877b40e5083404fabcf7244d2c2b3288
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2020-11-18 09:09:29 +0530

    Home screen UI and functionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,110 @@
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
 import {
- Text,StyleSheet,ScrollView
+ StyleSheet,ScrollView
 } from "react-native";
 
-import {Fab, Icon} from "native-base";
+import {
+  Fab,
+  Text,
+  Icon,
+  List,
+  ListItem,
+  Left,
+  Button,
+  Body,
+  Right,
+  CheckBox,
+  Title,
+  H1,
+  Subtitle,
+  Container,
+  Spinner
+} from "native-base";
+
+import AsyncStorage from "@react-native-community/async-storage";
+import {useIsFocused} from "@react-navigation/native";
+
 
 
 const Home = ({navigation,route}) => {
+
+    const [ListOfSeasons,setListOfSeasons] = useState([]);
+    const [loading,setLoading] = useState(false);
+    const isFocused = useIsFocused();
+
+    const getList = async () => {
+      setLoading(true);
+      const storedValue = await AsyncStorage.getItem('@season_list');
+      if(!storedValue){
+          setListOfSeasons([]);
+      }
+      const list = JSON.parse(storedValue)
+      setListOfSeasons(list);
+      setLoading(false);
+      
+    }
+    const deleteSeason = async () => {
+      //
+    }
+    const markComplete = async () => {
+      //
+    }
+
+    useEffect(()=>{
+      getList();
+    },[isFocused])
+   if(loading){
+     return(
+       <Container style={styles.container}>
+          <Spinner color="#00b7c2"></Spinner>
+       </Container>
+     )
+   }
+
     return (
         <ScrollView
         contentContainerStyle={styles.container}
         >
-            <Text style={{color:"white"}}>List Of Seasons</Text>
+          {ListOfSeasons.length == 0 ? (
+            <Container style={styles.container}>
+              <H1 style={styles.heading}>WatchList is empty</H1>
+            </Container>
+          ):(
+            <>
+            <H1 style={styles.heading}>Next Season to Watch</H1>
+            <List>
+              {ListOfSeasons.map((season)=>(
+                <ListItem key={season.id} style={styles.listItem} noBorder>
+                <Left>
+                  <Button danger style={styles.actionButton}>
+                  <Icon name="trash" active />
+                  </Button>
+                  <Button  style={styles.actionButton}>
+                   <Icon active name="edit" type="Feather" />
+                  </Button>
+                  
+                </Left>
+                <Body>
+                  <Title style={styles.seasonName}>
+                    {season.name}
+                  </Title>
+                  <Text note >
+                    {season.totalNoSeason}
+                  </Text>
+                </Body>
+                <Right>
+                  <CheckBox 
+                    
+
+                  />
+                </Right>
+              </ListItem>
+              ))}
+            </List>
+            </>
+            
+          )}
+            
             <Fab
             style={{backgroundColor:"#5067FF"}}
             position="bottomRight"
